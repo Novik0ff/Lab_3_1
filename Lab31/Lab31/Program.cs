7eafb76a860fe8f58effbef2deeae3af14a65817
= <=>
2019-04-22 14:52:10 +0300

    fin

--- Lab31/Lab31/Program.cs
+++ Lab31/Lab31/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
 
 namespace Lab31
 {
@@ -6,7 +8,23 @@ namespace Lab31
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QueryExpression qe = new QueryExpression("contact");
+            ColumnSet cs = new ColumnSet();
+            FilterExpression fe = new FilterExpression();
+            //ConditionExpression ce = new ConditionExpression();
+            cs.AddColumns("fullname", "parentcustomerid");
+            //ce.AttributeName = "jobtitle";
+            //ce.Operator = ConditionOperator.Equal;
+            //ce.Values.Add("Менеджер по закупкам");
+            //fe.AddCondition(ce);
+            qe.ColumnSet = cs;
+            qe.Criteria.AddFilter(fe);
+            EntityCollection contacts = Service.Service.GetOrganization().RetrieveMultiple(qe);
+            foreach (var item in contacts.Entities)
+            {
+                Console.WriteLine(item.Attributes["fullname"].ToString());
+            }
+            Console.Read();
         }
     }
 }
